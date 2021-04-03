680b36f9d055f72b6c71b6ce8ea7a87c7cb974ad
adam91091 <adam.kufel2@gmail.com>
2021-04-03 18:21:57 +0200

    Extend order status filtering by open or started option; Add custom choice filter with implementation for handling open or started option;

--- apps/orders/filters.py
+++ apps/orders/filters.py
@@ -1,6 +1,31 @@
 import django_filters
+from django.db.models import Q
 
 from apps.orders.models import Order
+
+ORDER_STATUS_OPEN_STARTED = 'Started or Open'
+
+
+class StatusChoiceFilter(django_filters.ChoiceFilter):
+    """Custom choice filter for order status"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra['choices'] += [
+            (ORDER_STATUS_OPEN_STARTED, ORDER_STATUS_OPEN_STARTED),
+        ]
+
+    def filter(self, qs, value: str):
+        """
+        Extend base filter method for retrieving opened or started orders.
+        Make filtering using Q query with alternative.
+        :param qs:    QuerySet instance
+        :param value: Value from extra['choices']
+        """
+        if value == ORDER_STATUS_OPEN_STARTED:
+            _qs = qs.filter(Q(status__iexact='Started') | Q(status__iexact='Open'))
+            return _qs
+        else:
+            return super().filter(qs, value)
 
 
 class OrderFilter(django_filters.FilterSet):
@@ -9,7 +34,7 @@ class OrderFilter(django_filters.FilterSet):
     product_sap_id = django_filters.CharFilter(field_name='product__product_sap_id', lookup_expr='icontains')
     date_of_production = django_filters.DateTimeFromToRangeFilter(lookup_expr='range')
     description = django_filters.CharFilter(field_name='product__description', lookup_expr='icontains')
-    status = django_filters.ChoiceFilter(choices=Order.STATUS_CHOICES, lookup_expr='iexact')
+    status = StatusChoiceFilter(choices=Order.STATUS_CHOICES, lookup_expr='icontains')
 
     class Meta:
         model = Order
